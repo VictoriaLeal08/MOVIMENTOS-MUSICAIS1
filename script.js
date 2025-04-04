8562e3541cc270bfc36c2e00fd3d47bfe3999033
VictoriaLeal08 <victoria.matos.leal@escola.pr.gov.br>
2025-04-04 02:11:37 +0000

    03 abril

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-document.addEventListener("DOMContentLoaded") , function(){
+document.addEventListener('DOMContentLoaded', function (){
   let tamanhoAtualFonte = 1;
+const botaoAumentarFonte = document.getElementById('aumentar-fonte');
+const botaoDiminuirFonte = document.getElementById('diminuir-fonte');
 
-  const botaoAumentarFonte = document.getElementById('aumentar-fonte');
-
-  const botaoDiminuirFonte = document.getElementById('diminuir-fonte');
-
-
-  botaoAumentarFonte.addEventListener('click', function(){
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
-    botaoAumentarFonte.addEventListener('click', function(){
-      tamanhoAtualFonte -= 0.1;
-      document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
-    
-  });
-  
+ botaoAumentarFonte.addEventListener('click', function(){
+   tamanhoAtualFonte += 0.1;
+   document.body.style.fontSize = ${tamanhoAtualFonte}rem;
+ });
+  botaoDiminuirFonte.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = ${tamanhoAtualFonte}rem;
+  })
 });
